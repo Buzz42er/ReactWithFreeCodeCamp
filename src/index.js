d8e2d2a3f3a6e07ad65e5dc99ffffca44b323f61
Buzz <jure.eres42@gmail.com>
2022-08-05 21:44:07 +0200

    leveling the play feild
    deleted all that I don't need for now and did a couple of components in index.js for now, later they will all be separated, but this was just to see how it all works

--- src/index.js
+++ src/index.js
@@ -1,17 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"; //ES6 moduals, imported React
+import { createRoot } from 'react-dom/client'; //react 18 syntax before it was reactDOM
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+//need to capitalise first letter of the name of the function
+function Greeting() {
+  //this is JSX
+  return (
+    <h1>Hello, This is react site</h1>
+  )
+}
+
+function List() {
+  return (
+    <ul>
+      <li>first</li>
+      <li>second</li>
+    </ul>
+  )
+}
+
+function Navi() {
+  return (
+    <nav>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+  )
+}
+
+const simple = ( //if appended, it will show an object, because jsx makes an object in JSON? or because it's inside of parentesies but it makes JSON object
+  <div>
+    <h1>This is first element</h1>
+    <p>this is second</p>
+    <ol>
+      <li>prvi</li>
+      <li>drugi</li>
+    </ol>
+    <p>ponestalo mi ideja, al to je cetiri</p>
+  </div>
+)
+
+// document.getElementById('root').append(JSON.stringify(simple))
+// getting JSON object
+
+// pass component to render, and get root element to display it
+// react 18 syntax, container and root
+const container = document.getElementById('root')
+const root = createRoot(container)
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  <div>
+    <Greeting /> 
+    <Navi/>
+    <List/>
+  </div>
+  // simple // var render
+  );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
