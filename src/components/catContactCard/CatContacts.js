79b864a010623fb229b1723774c8c4f84884dfc2
Buzz <jure.eres42@gmail.com>
2022-08-14 00:38:27 +0200

    0.1.6

--- src/components/catContactCard/CatContacts.js
+++ src/components/catContactCard/CatContacts.js
@@ -1,32 +1,33 @@
 import React from 'react'
 import ContactCard from './ContactCard'
+import pumpkin from '"./catsImg/pumpkin.png"'
 
 const CatContacts = () => {
     return (
         <div className="contacts">
             <ContactCard 
-                img="../../catsImg/mr-whiskerson.jpg"
+                img="./catsImg/mr-whiskerson.jpg"
                 name="Mr. Whiskerson"
                 phone="(212) 555-1234"
                 mail="mr.whiskaz@catnap.meow"   
             />
             
             <ContactCard 
-                img="../../catsImg/fluffykins.jpg"
+                img="./catsImg/fluffykins.jpg"
                 name="Fluffykins"
                 phone="(212) 555-2345"
                 mail="fluff@me.com"
             />
             
             <ContactCard 
-                img="../../catsImg/felix.jpg"
+                img="./catsImg/felix.jpg"
                 name="Felix"
                 phone="(212) 555-4567"
                 mail="thecat@hotmail.com"
             />
             
             <ContactCard 
-                img="../../../src/catsImg/pumpkin.png"
+                img={pumpkin}
                 name="Pumpkin"
                 phone="(0800) CAT KING"
                 mail="pumpkin@scrimba.com"
